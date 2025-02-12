d6f6fe33abf3685de330b9f56052c5b9862cdaf8
ehanuliakova <146837085+ehanuliakova@users.noreply.github.com>
2025-02-12 08:56:39 +0100

    Update polevka.py

--- polevka.py
+++ polevka.py
@@ -1,14 +1,25 @@
-from bs4 import BeautifulSoup
+ from bs4 import BeautifulSoup
 import requests
+import json
 
 
 def main():
 
+    url = "https://js-trebesin.github.io/bsoup-exam/"
+    response = requests.get(url) #get je python fce 
 
-    # response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")  # Pomocí knihovny BeautifulSoup, chceme obsah (content) response. Response nám odkazije na url adresu. "html parser" přetvoří/púřepíše text, aby mu kod rozuměl.
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    food = soup.find_all("p")
 
+    list_food = ["kmín", "česnek", "voda", "sůl"]
+
+    for p in food:
+        list_food.append(p.text)
+    
+    with open("polevka.json", mode = "w") as soubor:
+        json.dump(list_food, soubor, indent = 4, ensure_ascii = False)
+    print(list_food)
 
 if __name__ == "__main__":
     main()
