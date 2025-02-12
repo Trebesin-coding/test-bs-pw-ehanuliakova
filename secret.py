457907dc7968f4a0cb6948ce5ba6039dffe2ca6d
ehanuliakova <146837085+ehanuliakova@users.noreply.github.com>
2025-02-12 08:56:55 +0100

    Update secret.py

--- secret.py
+++ secret.py
@@ -2,20 +2,30 @@ from playwright.sync_api import sync_playwright
 import os
 
 
-
+jmeno = "Jarmil"
+heslo = "Admin123"
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
 
-        # browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
+        page=browser.new_page()
 
-        # !!!
-        # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
-        # !!!
+        page.goto("https://js-trebesin.github.io/playwright-exam/")
+        page.fill('input[id="login"]', jmeno)
+        page.fill('input[id="pass"]', heslo)
+        page.click('button')
 
+      
+        number = page.locator('p[class="super-secret-text"]').text_content()
+      
+         
+        print(number)
+ 
 
-        # browser.close()
+        input("vypni")
+        browser.close()
     
 
 if __name__ == "__main__":
