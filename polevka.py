ab752722c7f661791ff8f5cd323b04ff3158ac8a
JS <souhrada@trebesin.cz>
2025-03-20 21:36:05 +0100

    +hodnocení

--- polevka.py
+++ polevka.py
@@ -1,4 +1,4 @@
- from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 import json
 
@@ -6,20 +6,26 @@ import json
 def main():
 
     url = "https://js-trebesin.github.io/bsoup-exam/"
-    response = requests.get(url) #get je python fce 
+    response = requests.get(url)  # get je python fce <--- U: ?
 
-    soup = BeautifulSoup(response.content, "html.parser")  # Pomocí knihovny BeautifulSoup, chceme obsah (content) response. Response nám odkazije na url adresu. "html parser" přetvoří/púřepíše text, aby mu kod rozuměl.
+    soup = BeautifulSoup(
+        response.content, "html.parser"
+    )  # Pomocí knihovny BeautifulSoup, chceme obsah (content) response. Response nám odkazije na url adresu. "html parser" přetvoří/púřepíše text, aby mu kod rozuměl.
 
     food = soup.find_all("p")
+    # U: na stránce jsou nějaké <p> ?
 
     list_food = ["kmín", "česnek", "voda", "sůl"]
+    # U: správné ingredience měly být programátorky vyfiltrované ze všech prvků stránky, ne vytvořené staticky
 
     for p in food:
         list_food.append(p.text)
-    
-    with open("polevka.json", mode = "w") as soubor:
-        json.dump(list_food, soubor, indent = 4, ensure_ascii = False)
+
+    with open("polevka.json", mode="w") as soubor:
+        json.dump(list_food, soubor, indent=4, ensure_ascii=False)
     print(list_food)
+    # U: do konzole měl být vypsán text z prvků, ne celý list
+
 
 if __name__ == "__main__":
     main()
--- secret.py
+++ secret.py
@@ -5,28 +5,26 @@ import os
 jmeno = "Jarmil"
 heslo = "Admin123"
 
+
 def main():
 
     with sync_playwright() as p:
 
         browser = p.chromium.launch(headless=False)
-        page=browser.new_page()
+        page = browser.new_page()
 
         page.goto("https://js-trebesin.github.io/playwright-exam/")
         page.fill('input[id="login"]', jmeno)
         page.fill('input[id="pass"]', heslo)
-        page.click('button')
+        page.click("button")
 
-      
         number = page.locator('p[class="super-secret-text"]').text_content()
-      
-         
+
         print(number)
- 
 
         input("vypni")
         browser.close()
-    
+
 
 if __name__ == "__main__":
     main()
